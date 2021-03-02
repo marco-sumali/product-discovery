508e5740039442264bed442a481cd85b6b4c0584
Marco Sumali <marco.sumali@2359media.com>
2021-03-03 00:36:52 +0700

    chore: add functions and revise client

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,28 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp()
+// Node fetch initialisation
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.nodeFetch = functions.https.onRequest((req, res) => {
+  fetch('https://www.revolut.com/help')
+    .then(data => data.text())
+    .then(body => {
+      console.log('check:0', body)
+      const load = cheerio.load(body);
+      console.log('check:', load)
+      res.send({message: body})
+
+    });
+});
+
+
