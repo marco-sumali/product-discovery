19bafb70c35739ae847fba6823e7c37bce967ab4
Marco Sumali <marco.sumali@2359media.com>
2021-03-03 01:14:50 +0700

    chore: add functions to send stringify html

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -5,7 +5,7 @@ const admin = require('firebase-admin');
 admin.initializeApp()
 // Node fetch initialisation
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+const DOMParser = require('dom-parser');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -16,12 +16,8 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.nodeFetch = functions.https.onRequest((req, res) => {
   fetch('https://www.revolut.com/help')
     .then(data => data.text())
-    .then(body => {
-      console.log('check:0', body)
-      const load = cheerio.load(body);
-      console.log('check:', load)
-      res.send({message: body})
-
+    .then(text => {
+      res.send({strHtml: text})
     });
 });
 
