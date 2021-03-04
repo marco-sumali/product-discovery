ae271f5d6af6d935f2f8729983f612e3a6f66674
Marco Sumali <marco.sumali@2359media.com>
2021-03-04 16:22:18 +0700

    chore: revise home

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -9,7 +9,7 @@ import styles from './style.js';
 const HomePage = () => {
   return (
     <NavigationBar>
-      <Grid container style={{backgroundColor: '#ffffff', minHeight: '100vh', paddingTop: '24px'}}>
+      <Grid container style={{backgroundColor: '#ffffff', minHeight: '100vh', paddingTop: '8px'}}>
         <Grid item xl={6} lg={6} md={12} sm={12} xs={12}>
           <div style={{height: '80vh'}}>
             <div style={styles.ContainerWrapCenter}>
@@ -20,8 +20,10 @@ const HomePage = () => {
             </div>
           </div>
         </Grid>
-        <Grid item xl={6} lg={6} md={12} sm={12} xs={12}>
-          <img style={styles.HomePhoneImage} src="https://assets.revolut.com/media/index/home.png" alt="home-phone"/>
+        <Grid item xl={6} lg={6} md={12} sm={12} xs={12} style={{marginTop: '-64px'}}>
+          <div style={styles.ContainerWrapCenter}>
+            <img style={styles.HomePhoneImage} src="https://assets.revolut.com/media/index/home.png" alt="home-phone"/>
+          </div>
         </Grid>
       </Grid>
     </NavigationBar>
